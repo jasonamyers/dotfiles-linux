6a9a0a56762a4c572c6218b06dab3f0817705e03
Jason Myers <jasonamyers@stratajam.local>
2013-06-17 18:22:04 -0500

    Removing emacs and oh-my-zsh
    Signed-off-by: Jason Myers <jasonamyers@stratajam.local>

--- emacs.d/elpa/pyflakes-1.0/pyflakes-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "pyflakes" "1.0" "run the python pyflakes checker putting hits in a grep buffer" (quote nil))
--- emacs.d/elpa/s-1.6.0/s-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "s" "1.6.0" "The long lost Emacs string manipulation library." (quote nil))
