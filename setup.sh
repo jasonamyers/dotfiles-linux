3d199db2c423875c56758dd6a0a60eeb646f43fe
Jason Myers <jason@jasonamyers.com>
2014-05-14 11:05:58 -0500

    Fixing missing /bin/bash
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo ">> Initialising git submodules"
+git submodule init
+git submodule update
+
+# Init symlinks for config files
+echo -e "\n>> Creating symlinks"
+for file in [a-zA-Z]* ; do
+    if [[ "$file" != "setup.sh" && "$file" != "README.md" && "$file" != "env" && "$file" != "LICENSE.txt" ]] ; then
+        if [[ -h ~/."$file" ]] ; then
+            # symlink, remove it since it will be re-created anyway
+            echo "  Removing symbolic link ~/.$file"
+            rm ~/."$file"
+        elif [[ -a ~/."$file" ]] ; then
+            # an actual file, create backup
+            echo "  ~/.$file exists. Moving it to ~/.$file.backup"
+            mv ~/."$file" ~/."$file".backup || echo "  Couldn't move ~/.$file to ~/.$file.backup!"
+        fi
+
+        ln -s `pwd`/"$file" ~/."$file" && echo "  Linked ~/.$file to `pwd`/$file" || echo "  Couldn't link ~/.$file to `pwd`/$file!"
+    fi
+done
+
+# Init vim bundles
+echo ">> Install vim bundles using Vundle"
+vim +BundleInstall +qall
